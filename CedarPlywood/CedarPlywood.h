dd8a3f6e718487e65a1be988661a8180f980984f
Jeff Hui <jeff@jeffhui.net>
2013-03-20 21:36:02 -0700

    Initial Commit

---
+++ CedarPlywood/CedarPlywood.h
@@ -0,0 +1,13 @@
+//
+//  CedarPlywood.h
+//  CedarPlywood
+//
+//  Created by Jeff Hui on 3/20/13.
+//  Copyright (c) 2013 Jeff Hui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CedarPlywood : NSObject
+
+@end
---
+++ CedarPlywood/CedarPlywood.m
@@ -0,0 +1,13 @@
+//
+//  CedarPlywood.m
+//  CedarPlywood
+//
+//  Created by Jeff Hui on 3/20/13.
+//  Copyright (c) 2013 Jeff Hui. All rights reserved.
+//
+
+#import "CedarPlywood.h"
+
+@implementation CedarPlywood
+
+@end
